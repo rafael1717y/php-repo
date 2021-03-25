ac14b7eeefcd71abd1b4a830369a92a51fa00946
rafael1717y <rafaelufjf@gmail.com>
2021-03-25 20:03:51 -0300

    listas

---
+++ listas.php
@@ -0,0 +1,12 @@
+<?php
+
+$idades = [10, 20, 32, 22, 31];
+
+$primeiraIdade =  $idades[0];
+
+//echo $primeiraIdade;
+
+for ($i = 0; $i < count($idades); $i++) {
+    echo $idades[$i] . PHP_EOL;
+}
+
