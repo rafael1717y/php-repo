8545f1977a0ba8659685ec2a6d69c1c24eb378d0
rafael1717y <rafaelufjf@gmail.com>
2021-05-29 08:27:30 -0300

    exercicios

---
+++ 01.curso1_php/04.curso_treehouse_php/integer.php
@@ -0,0 +1,60 @@
+<?php
+
+$num_one = 1;
+$number_two = 2;
+$number_three = 3;
+
+echo $num_one;
+echo 1;
+echo "1";
+
+// Tipos de dados
+var_dump($num_one);
+var_dump(1);
+var_dump("1");
+var_dump($num_one + $number_two - $number_three);
+
+$distance_home = 1.2;
+$distance_work = 2.5;
+
+
+var_dump($distance_home + $distance_work + $number_three + .3);
+
+$a = 5;
+$b = 10;
+
+var_dump($a * $b);
+var_dump($a / $b);
+
+$a = $a + 1;
+var_dump($a);
+$a++;
+var_dump($a);
+$a--;
+var_dump($a);
+var_dump($a--);
+var_dump($a);
+
+$a = $a + 5;
+var_dump($a);
+
+$a +=5;
+var_dump($a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ 01.curso1_php/04.curso_treehouse_php/units.php
@@ -0,0 +1,38 @@
+<?php
+// number in pounds we want to convert do kilograms
+$pounds = 140;
+
+// float point value for the pound to kilogram conversion
+$lb_to_kg = 0.45392;
+
+// use the variables above to calculate pounds multiplied by the kilogram conversion
+$kilograms = $pounds * $lb_to_kg;
+
+// display the pounds to kilogram
+echo "Weight: ";
+echo $pounds;
+echo " lb =";
+echo $kilograms;
+echo " kg";
+
+
+// number in miles we want to convert do kilometers
+$miles = 2.5;
+
+// floating point value for the mile to kilometer conversation
+$mile_to_km = 1.60934;
+
+// use the variable above to calculate miles * kilometer conversion
+$kilometers = $miles * $mile_to_km;
+
+// display
+echo "Distance: ";
+echo " miles = ";
+echo $kilometers;
+echo " km";
+
+
+
+
+
+?>
