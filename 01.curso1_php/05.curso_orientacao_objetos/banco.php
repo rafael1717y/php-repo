8f2d946cde139aeb38f2233c4956baad39f11527
rafael1717y <rafaelufjf@gmail.com>
2021-06-08 13:21:28 -0300

    orientacao_objetos_php

---
+++ 01.curso1_php/05.curso_orientacao_objetos/banco.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'Conta.php';
+require_once 'Titular.php';
+require_once 'CPF.php';
+
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$primeiraConta = new Conta($vinicius);
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300); // isso é ok
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$segundaConta = new Conta($patricia);
+var_dump($segundaConta);
+
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+unset($segundaConta);
+echo Conta::recuperaNumeroDeContas();
